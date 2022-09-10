da3ea9e0398250aa32356470b552808b06f26807
alex0109 <krossyouyub@gmail.com>
2022-09-10 23:34:03 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React , {useState} from 'react';
 import './App.css';
 
-function App() {
+const data = [
+  { name: "Landing Page", time: 40 },
+  { name: "Configurator", time: 79 },
+  { name: "Check-out", time: 16 },
+  { name: "Deal", time: 28 }
+];
+
+const App = () => {
+
+  const [timeData, setTimeData] = useState(data);
+  const maxTime = 100;
+
+  const setRandomData = (data) => data.map((num) => ({
+    name: num.name,
+    time: Math.round(Math.random() * maxTime)
+  }))
+
+  let chartHandleRefresh = () => {
+    const newData = setRandomData(timeData);
+    setTimeData(newData);   
+  }
+
+  const setPercentWidth = (currTime) => {
+    const perWidth = (Math.ceil(((currTime) * 100)/maxTime))
+    return perWidth
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='container'>
+      {timeData.map((data) => {
+        const currentWidth = setPercentWidth(data.time)
+        return (
+          <div className='item'>
+            <p>{data.name}</p>
+            <div className='bar'>
+              <div className='progress' style={{
+                width: `${currentWidth}%`
+              }}>{data.time}</div>
+              </div>
+          </div>
+        );
+      })}
+      </div>
+
+      <button onClick={chartHandleRefresh}>Refresh Chart</button>
+
+    </>
   );
 }
+
 
 export default App;
